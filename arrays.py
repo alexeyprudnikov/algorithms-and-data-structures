3a7816c443c16a28af2015ae22c197f47f9163b7
alexeyprudnikov <alexey.prudnikov@yahoo.de>
2021-08-25 13:28:06 +0200

    bubble sort

--- arrays.py
+++ arrays.py
@@ -8,19 +8,22 @@ def start():
     print('Input array:')
     print(input_array)
     print('Selection sort:')
-    output_array = selection_sort(input_array)
-    print(output_array)
+    output_array_1 = selection_sort(input_array)
+    print(output_array_1)
     print('Selection sort recursive:')
-    output_array = selection_sort_recursive(input_array)
-    print(output_array)
+    output_array_2 = selection_sort_recursive(input_array)
+    print(output_array_2)
+    print('Bubble sort:')
+    output_array_3 = bubble_sort(input_array)
+    print(output_array_3)
 
 
 # selection sort
 def selection_sort(array):
-    # init min index as first index
-    min_index = 0
-    # go through array
+    # loop through array
     for i in range(len(array)):
+        # init min index as first index
+        min_index = i
         # find index of min value in rest array
         for j in range(i+1, len(array)):
             if array[j] < array[min_index]:
@@ -46,3 +49,18 @@ def selection_sort_recursive(array, first_index=0):
     if first_index < len(array) - 2:
         array = selection_sort_recursive(array, first_index + 1)
     return array
+
+
+#bubble sort
+def bubble_sort(array):
+    # loop through array
+    for i in range(len(array)):
+        try:
+            # next index
+            j = i + 1
+            # if current value > next value, swap elements
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+        except IndexError:
+            # if next list index does not exists -> end of list, return
+            return array
